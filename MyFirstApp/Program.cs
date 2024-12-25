01eb99d814c61d6e97a81a5729c31efb6493d55a
Teegrius <tatosian.tigr@yandex.ru>
2024-12-25 21:52:31 +0200

    Добавьте файлы проекта.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,49 @@
+﻿class Programm
+{
+
+    static void Main(string[] args)
+    {
+       if (Int32.TryParse(Console.ReadLine(), out var a))
+        {
+            Console.WriteLine("Not a number!");
+            return;
+        }
+
+       if (Int32.TryParse(Console.ReadLine(),out var b))
+        { 
+            Console.WriteLine("Not a number");
+            return;
+        }
+
+        var s = Console.ReadLine();
+        var boolVar = true;
+        if (s.Length == 0 || s.Length > 1 && !boolVar) 
+        {
+            Console.WriteLine("Wrong sign");
+            return;
+        }
+
+        switch (s[0])
+        {
+            case '+':
+                Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+                break;
+            case '-':
+                Console.WriteLine("Result of {0} + {1} = {2}", a, b, a - b);
+                break;
+            case '*':
+                Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+                break;
+            case '/':
+                Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
+                break;
+            default:
+                Console.WriteLine("Wrong sigh");
+                break;
+
+       
+
+        }
+
+    }
+}    
